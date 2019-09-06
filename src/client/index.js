6644919bac42a6cc58e661af0f61cfa9a8adbe65
HaoyangLiu <lhy.0318@163.com>
2019-09-06 19:57:43 +0800

    Add timeRelock and timeUnlock

--- src/client/index.js
+++ src/client/index.js
@@ -549,7 +549,6 @@ export class BncClient {
     }
 
     const tx = new Transaction(options)
-    console.log(tx)
     return this._signingDelegate.call(this, tx, stdSignMsg)
   }
 
@@ -561,7 +560,6 @@ export class BncClient {
    */
   async sendTransaction(signedTx, sync) {
     const signedBz = signedTx.serialize()
-    console.log(signedBz)
     return this.sendRawTransaction(signedBz, sync)
   }
 
